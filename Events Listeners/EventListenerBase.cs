3fa30e27cd65854c3a1f29bc50407dcba211f949
Arekushi <alex.ferreiradelima64@gmail.com>
2020-04-30 16:55:01 -0300

    Monobehaviour Event Listener Base
    This is the base class for your daughters.
    
    It has 3 methods, precisely to register the methods that will be called, remove them and finally execute the event, thus calling all registered methods.
    
    So, here we see, I can use the same code for the same GameObjects and they will be executed only by themselves, without affecting others in the scene.

---
+++ Events Listeners/EventListenerBase.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+namespace Arekushi.UnityEvent
+{
+
+    public abstract class EventListenerBase<T> : MonoBehaviour
+    {
+
+        private readonly Dictionary<GameObject, UnityEventBase<T>> listenerEvent = 
+            new Dictionary<GameObject, UnityEventBase<T>>();
+
+        public void RegisterEvent(GameObject ownerEvent, UnityAction<T> action)
+        {
+            listenerEvent.TryGetValue(ownerEvent, out UnityEventBase<T> unityEvent);
+
+            if (unityEvent == null)
+            {
+                unityEvent = new UnityEventBase<T>();
+                listenerEvent[ownerEvent] = unityEvent;
+            } 
+
+            unityEvent.AddListener(action);
+        }
+
+        public void UnRegisterEvent(GameObject ownerEvent, UnityAction<T> action)
+        {
+            if (listenerEvent.TryGetValue(ownerEvent, out UnityEventBase<T> unityEvent))
+            {
+                if (unityEvent != null) unityEvent.RemoveListener(action);
+            }
+        }
+
+        public void ExecuteEvent(GameObject ownerEvent, T data) {
+            if (listenerEvent.TryGetValue(ownerEvent, out UnityEventBase<T> unityEvent))
+            {
+                if (unityEvent != null) unityEvent.Invoke(data);
+            }
+        }
+
+    }
+}
