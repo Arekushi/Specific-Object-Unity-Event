756418ed2e05385a9745ecaedee9900a432ea726
Arekushi <alex.ferreiradelima64@gmail.com>
2020-04-30 16:56:39 -0300

    Event Listener Hit
    This is a child of the base class (EventListenerBase), but here we have a type pass, which is HitData, the type I want the methods to have as a parameter.

---
+++ Events Listeners/EventListenerHit.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+namespace Arekushi.UnityEvent
+{
+
+    public class EventListenerHit : EventListenerBase<HitData> { }
+}
