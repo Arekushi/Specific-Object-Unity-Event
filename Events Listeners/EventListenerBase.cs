e2b7b68d32f705f30af48657ceb5cf2cc35ef77d
Arekushi <alex.ferreiradelima64@gmail.com>
2020-04-30 17:45:53 -0300

    Monobehaviour Event Listener Base

--- Events Listeners/EventListenerBase.cs
+++ Events Listeners/EventListenerBase.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine.Events;
 
-namespace Arekushi.UnityEvent
+namespace Arekushi.SpecificUnityEvent
 {
 
     public abstract class EventListenerBase<T> : MonoBehaviour
