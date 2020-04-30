74a9642d1ab7ee47666868ad2d4f82a3312abb14
Arekushi <alex.ferreiradelima64@gmail.com>
2020-04-30 16:39:45 -0300

    Class Data
    This class is a class to use as a parameter.

---
+++ Data/HitData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Arekushi.UnityEvent
+{
+    public class HitData
+    {
+
+        public GameObject aggressor;
+        public GameObject victim;
+        public int damage;
+
+        public HitData(GameObject aggressor, GameObject victim, int damage)
+        {
+            this.aggressor = aggressor;
+            this.victim = victim;
+            this.damage = damage;
+        }
+    }
+
+}
